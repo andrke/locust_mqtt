d57d8e4cfea175b057adab269badd5007e6875eb
Andrew Chen <yongjhih@gmail.com>
2018-03-08 22:34:02 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='locust_mqtt',  # Required
+    name='locust-mqtt',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
